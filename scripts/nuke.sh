e4f6ee035992589a3cf5d8514660eee6b57bf39c
Frank Purcell <fxpurcell@gmail.com>
2020-07-21 17:51:59 -0700

    fix(submod): add openmaptiles submod

---
+++ scripts/nuke.sh
@@ -0,0 +1,29 @@
+DIR=`dirname $0`
+
+echo kill & down containers 
+docker kill $(docker ps -aq)
+docker-compose down
+wait;
+
+if [[ $1 == 'ALL' ]]
+then
+    echo REMOVE all containers
+    echo
+    docker ps --all --quiet --no-trunc --filter "status=exited" | xargs --no-run-if-empty docker rm
+    docker images --quiet --filter "dangling=true" | xargs --no-run-if-empty docker rmi
+    docker rm $(docker ps -a -q)
+    docker rmi $(docker images -q)
+    docker volume ls | awk '{print $2}' | xargs docker volume rm
+
+    echo
+    echo
+    docker ps
+    echo
+    echo
+    docker images
+    echo
+    echo
+    docker volume ls
+    echo
+    echo
+fi
