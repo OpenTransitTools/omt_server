10bc2d58cd5985e989f0cec31d321e136ba37ba7
Frank Purcell <fxpurcell@gmail.com>
2020-08-27 22:17:17 -0700

    fix(scripts): have env var control max zoom

--- scripts/env_update.sh
+++ scripts/env_update.sh
@@ -1,8 +1,11 @@
 DIR=`dirname $0`
 FILE=$DIR/../openmaptiles/.env
+MAX_ZOOM=${MAX_ZOOM:=16}
 
 # bkup file
-mv $FILE $FILE.orig
+#mv $FILE $FILE.orig
 
 # make change(s) to the openmaptiles/.env file via 'sed'
-cat $FILE.orig | sed 's/MAX_ZOOM=7/MAX_ZOOM=15/g' > $FILE
+SED_CHG_ZOOM="s/MAX_ZOOM=7/MAX_ZOOM=${MAX_ZOOM}/g"
+echo "sed $SED_CHG_ZOOM > $FILE"
+cat $FILE.orig | sed $SED_CHG_ZOOM > $FILE
