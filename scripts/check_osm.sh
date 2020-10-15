3e5d446ef816313df6faf3284b4b0f3ee22757a6
Frank Purcell <fxpurcell@gmail.com>
2020-10-14 17:02:20 -0700

    fix(script): osm update / rebuild script 1/?

--- scripts/check_osm.sh
+++ scripts/check_osm.sh
@@ -1,30 +1,68 @@
 #
 # check_osm.sh
 #
-# will look at OSM file and determine if the tiles should
-# be updated...
+# will download the OSM meta file, compare it against a cached version, and if 
+# different, download the new OSM data...
 #
+SERVER="http://maps6.trimet.org"
+OSM_META_FILE="or-wa.osm-stats"
+OSM_FILE="or-wa-carto.osm.pbf"
+OSM_META_URL="$SERVER/pelias/$OSM_META_FILE"
+OSM_DATA_URL="$SERVER/pelias/$OSM_FILE"
+DATA_DIR=~/omt_server/openmaptiles/data
 
-OSM_FILE=or-wa.osm.pbf
-OSM_URL="http://maps6.trimet.org/pelias/$OSM_FILE"
-curl $OSM_URL > ~/$OSM_FILE
 
+function update_osm_data() {
+  old_osm_dir="$DATA_DIR/../old/"
 
-function check_osm() {
-    # returns true if the old and new gtfs files don't match
-    ret_val=0
-    trans_dir=$1
-    tmp_dir=$trans_dir/tmp
+  # step 1: move old OSM data aside
+  if [ ! -d $DATA_DIR ]; then
+    mkdir -p $DATA_DIR
+  else
+    rm -rf $old_osm_dir
+    mkdir -p $old_osm_dir
+    mv $DATA_DIR/$OSM_META_FILE $old_osm_dir
+    mv $DATA_DIR/$OSM_FILE $old_osm_dir
+    mv $DATA_DIR/*mbtiles $old_osm_dir
+  fi
 
-    DF=`diff $trans_dir/feed_info.txt $tmp_dir/feed_info.txt`
-    if [ -z "$DF"  ]; then
-        echo "feeds match ... not reloading"
-        ret_val=0
-    else
-        echo "feeds DO NOT match ... diff of feed_info: $DF"
-        ret_val=1
-    fi
-    return $ret_val
+  # step 2: grab new data
+  curl $OSM_META_URL > $DATA_DIR/$OSM_META_FILE
+  curl $OSM_DATA_URL > $DATA_DIR/$OSM_FILE
 }
 
 
+function check_osm_meta_data() {
+  # returns true if the old and new gtfs files don't match
+  ret_val=0
+
+  if [ -d $DATA_DIR ]; then
+
+    # step 1: grab meta data into tmp dir
+    tmp_dir="$DATA_DIR/tmp"
+    rm -rf $tmp_dir
+    mkdir $tmp_dir
+    curl $OSM_META_URL > $tmp_dir/$OSM_META_FILE
+
+    # step 2: compare new meta data vs. old  
+    DF=`diff $DATA_DIR/$OSM_META_FILE $tmp_dir/$OSM_META_FILE`
+    if [ -z "$DF"  ]; then
+	echo "OSM data match ... not reloading"
+	ret_val=0
+    else
+	echo "OSM (meta) data DOES NOT match (eg: $DF)"
+        mv $tmp_dir/$OSM_META_FILE $DATA_DIR
+	ret_val=1
+    fi
+    #rm -rf $tmp_dir
+  fi
+
+  return $ret_val
+}
+
+# update data 
+new=check_osm_meta_data
+echo $new
+if [ $new != 1 ]; then
+  update_osm_data
+fi
