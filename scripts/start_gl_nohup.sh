f17ec7bdf2108a69d4f3901b07182fffc224aa65
Frank Purcell <fxpurcell@gmail.com>
2020-10-15 16:49:02 -0700

    fix(script): fix run.sh script to find mbtiles junk

--- scripts/start_gl_nohup.sh
+++ scripts/start_gl_nohup.sh
@@ -1,3 +1,5 @@
 DIR=`dirname $0`
 cd $DIR/../gl/
-tmux new-session -d -s my_session 'run.sh >> out.txt 2>&1'
+cmd="tmux new-session -d -s my_session 'run.sh >> out.txt 2>&1'"
+echo "cd $PWD; $cmd"
+eval $cmd
