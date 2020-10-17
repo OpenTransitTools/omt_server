81fd947c1cb84ba985550b6eb3278cbd89d978b0
Frank Purcell <fxpurcell@gmail.com>
2020-10-17 12:44:49 -0700

    fix(script): more restart docker junk

--- scripts/docker-process-list.sh
+++ scripts/docker-process-list.sh
@@ -4,7 +4,11 @@ do
   ps="docker exec -it $c ps -aux"
   echo $ps
   eval $ps
+
+  insp="docker inspect $c"
   echo
   echo "docker exec -it $c kill -9 <pid>"
+  echo "$insp"
+  eval "$insp | grep -i restartcount"
   echo
 done
