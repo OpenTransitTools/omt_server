0805befe45db190079bcf62180158c95e044b45b
Frank Purcell <fxpurcell@gmail.com>
2020-11-02 15:25:38 -0800

    fix(scritps): bolt restart to remotely turn GL on

--- scripts/bolt/bolt-restart.sh
+++ scripts/bolt/bolt-restart.sh
@@ -1,3 +1,6 @@
+##
+## will remotely update and restart the GL server from omt_server project
+##
 BDIR=`dirname $0`
 . $BDIR/bolt-base.sh
 
@@ -6,5 +9,14 @@ do
   echo $m
 
   # step 1: update code (2nd call should show all was updated / nothing pending)
-  bolt command run 'ls -l ~/' --targets $m
+  bolt command run '~/omt_server/scripts/omt_update.sh' --targets $m
+  sleep 60
+
+  # step 2: check that GL docker is running 
+  bolt command run 'docker ps' --targets $m
+
+  # step 3: check that GL docker is running 
+  #TODO: grab a static image ... make sure it's valid and of size
+  #sleep 50
+  #curl $m:8080/
 done
