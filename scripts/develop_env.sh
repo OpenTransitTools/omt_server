c4a74d2b971aea22454314980bd81d9b40c2ff05
Frank Purcell <fxpurcell@gmail.com>
2021-02-22 22:06:13 -0800

    fix(build): dev fix

--- scripts/develop_env.sh
+++ scripts/develop_env.sh
@@ -1,5 +1,5 @@
 # source me for dev env
 echo "DEVELOPing..."
-export ROOT_DIR="/Users/fpurcell/java/DEV/omt_server"
+export ROOT_DIR="$HOME/java/DEV/omt_server"
 export OSM_SERVER="http://maps7.trimet.org"
 export START_GL_SH="start_gl.sh"  
