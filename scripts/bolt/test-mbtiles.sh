5b06e3634188bb91dbbe4601801afb4e0d8e29b3
Frank Purcell <fxpurcell@gmail.com>
2021-03-05 22:00:39 -0800

    curl test running well

--- scripts/bolt/test-mbtiles.sh
+++ scripts/bolt/test-mbtiles.sh
@@ -5,6 +5,24 @@ BDIR=`dirname $0`
 . $BDIR/bolt-base.sh
 . $BDIR/../base.sh
 
+# step 0: clear out tmp
+rm -rf /tmp/* > /dev/null 2>&1 
+
+
+function curl_test() {
+  cmd="curl $1 > /tmp/$2"
+  echo $cmd
+  eval $cmd > /dev/null 2>&1
+
+  size=`ls -ltr /tmp/$2 | awk -F" " '{ print $5 }'`
+  if [[ $size -gt 300000 ]]
+  then
+    echo "/tmp/$2 looks GOOD at size $size"
+  else
+    echo "/tmp/$2 seems SMALL at $size" 
+  fi
+}
+
 
 # step 1: loop thru machines
 for m in $MACHINES
@@ -12,16 +30,12 @@ do
   echo "testing GL on $m"
 
   # step 2: backup existing data dir 
-  TM_URL="http://$m:8080/styles/trimet/16/20873/46882@6x.jpg"
-  TM_SAT_URL="http://$m:8080/styles/trimet-satellite/16/20873/46882@6x.jpg
+  TM_MAP_URL="http://$m:8080/styles/trimet/13/1304/2930@6x.png"
+  TM_SAT_URL="http://$m:8080/styles/trimet-satellite/16/10435/23440@6x.png"
 
   # step 3: scp copy data dir to server
-  scp -r $GL_DATA_DIR $m:$GL_DATA_DIR
-done
+  curl_test $TM_MAP_URL $m-map.png
+  curl_test $TM_SAT_URL $m-sat.png
 
-size=`ls -ltr $MBTILES_PATH | awk -F" " '{ print $5 }'`
-if [[ $size -gt 10000000 ]]
-then
-else
-  echo "$MBTILES_PATH is too small at $size to copy over to $m"
-fi
+  echo ""
+done
