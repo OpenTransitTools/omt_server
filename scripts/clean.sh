823135626512fa822ff7c044cc5d278fb49f8f20
Frank Purcell <fxpurcell@gmail.com>
2021-10-11 15:36:32 -0700

    fix(script): update clean; update borders (from OMT)

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,9 +3,14 @@ DIR=`dirname $0`
 rm -f  $DIR/../cron_load.txt
 rm -f  $DIR/../gl/data/*
 rm -rf $DIR/../gl/data-bkup
-rm -f  $DIR/../openmaptiles/.env
+
 rm -rf $DIR/../openmaptiles/build
 rm -rf $DIR/../openmaptiles/cache
 rm -rf $DIR/../openmaptiles/data
+
+# reset openmaptiles
+cd $DIR/../openmaptiles/
+git reset --hard HEAD
+cd -
 
 $DIR/nuke.sh ALL
