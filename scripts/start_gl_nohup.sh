aa38225688674f984b35b40e86419dc8841bede2
Frank Purcell <fxpurcell@gmail.com>
2025-02-15 14:47:17 -0800

    fix: improve restart script

--- scripts/start_gl_nohup.sh
+++ scripts/start_gl_nohup.sh
@@ -1,6 +1,13 @@
 STDIR=`dirname $0`
+
 cd $STDIR/../
+
+# take down OMT
 docker-compose down
-cmd="tmux new-session -d -s gl_session 'docker-compose up tileserver-gl >> out.txt 2>&1'"
+docker system prune -a -f
+mv out.txt out.old
+
+# restart OMT
+cmd="docker-compose up -d tileserver-gl >> out.txt 2>&1"
 echo "cd $PWD; $cmd"
 eval $cmd
