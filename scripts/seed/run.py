13bbe8b3b7ea9667b11ede6b4c6b053ddafefdb0
Frank Purcell <fxpurcell@gmail.com>
2025-05-31 01:44:10 -0700

    fix(seed): better process of number of urls

--- scripts/seed/run.py
+++ scripts/seed/run.py
@@ -18,7 +18,7 @@ def get_cmdline_args(prog_name='seed.run', do_parse=True):
     parser.add_argument('--url',    '-u', type=str, default="http://tiles-st.trimet.org", help='url to seed')
     parser.add_argument('--style',  '-s', type=str, default="trimet", help='style')
     parser.add_argument('--wait',   '-w', type=int, default=3, help='time between layer requests')
-    parser.add_argument('--max',    '-m', type=int, default=100, help='limit to number of requests')
+    parser.add_argument('--max',    '-m', type=int, default=300, help='limit to number of requests')
     parser.add_argument('--retina', '-2x', action='store_true', help='creat the @2x tiles')
 
     if do_parse:
@@ -27,9 +27,27 @@ def get_cmdline_args(prog_name='seed.run', do_parse=True):
         args = parser
     return args
 
+
+def factor_bbox(minlat, maxlat, minlon, maxlon, factor=0.0):
+    minlat -= factor
+    maxlat += factor
+    minlon -= factor
+    maxlon += factor
+    return minlat, minlon, maxlat, maxlon
+
+
+def pt_2_bbox(lat, lon, factor=0.0):
+    return factor_bbox(lat, lat, lon, lon, factor)
     
-def seed_layer(minlat, minlon, maxlat, maxlon, zoomlevel, url, style, retina):
+
+def max_extent():
+    return factor_bbox(-45.0, 45.0, -180.0, 180.0)
+
+
+def get_seed_urls(minlat, minlon, maxlat, maxlon, zoomlevel, url, style, retina, max_urls):
     " https://tiles-st.trimet.org/styles/trimet/18/41742/93764@2x.png "
+    ret_val = []
+
     mercator = maptile.GlobalMercator()
 
     mx, my = mercator.LatLonToMeters( minlat, minlon )
@@ -37,21 +55,44 @@ def seed_layer(minlat, minlon, maxlat, maxlon, zoomlevel, url, style, retina):
 
     mx, my = mercator.LatLonToMeters( maxlat, maxlon )
     tmaxx, tmaxy = mercator.MetersToTile( mx, my, zoomlevel )
+    
+    ext = "@2x.png" if retina else ".png"
 
     for ty in range(tminy, tmaxy+1):
         for tx in range(tminx, tmaxx+1):
             gx, gy = mercator.GoogleTile(tx, ty, zoomlevel)
-            tilefilename = "{}/styles/{}/{}/{}/{}".format(url, style, zoomlevel, gx, gy)
-            if retina:
-                print (tilefilename + "@2x.png")
-            else:
-                print (tilefilename + ".png")
+            url = "{}/styles/{}/{}/{}/{}{}".format(url, style, zoomlevel, gx, gy, ext)
+            ret_val.append(url)
+        if len(ret_val) > max_urls:
+            break
+    return ret_val
+
+
+def crazy_extent_calc(zoom):
+    minlat, minlon, maxlat, maxlon = pt_2_bbox(args.lat, args.lon)
+    # crazy logic to extend the lat lon bbox and not genereate too many tiles
+    radius = args.radius
+    if radius > 1:
+        if zoom <= 4:
+            minlat, minlon, maxlat, maxlon = max_extent()
+        elif zoom < 7:
+            minlat, minlon, maxlat, maxlon = pt_2_bbox(args.lat, args.lon, radius * 10.00)
+        elif zoom < 10:
+            minlat, minlon, maxlat, maxlon = pt_2_bbox(args.lat, args.lon, radius * 1.75)
+        elif zoom < 13:
+            minlat, minlon, maxlat, maxlon = pt_2_bbox(args.lat, args.lon, radius * 0.17)
+        elif zoom < 16:
+            minlat, minlon, maxlat, maxlon = pt_2_bbox(args.lat, args.lon, radius * 0.03)
+        elif zoom < 18:
+            minlat, minlon, maxlat, maxlon = pt_2_bbox(args.lat, args.lon, radius * 0.007)
+        elif zoom < 20:
+            minlat, minlon, maxlat, maxlon = pt_2_bbox(args.lat, args.lon, radius * 0.0005)
+        elif zoom < 25:
+            minlat, minlon, maxlat, maxlon = pt_2_bbox(args.lat, args.lon, radius * 0.00005)
+    return minlat, minlon, maxlat, maxlon
 
 
 def runner(args):
-    minlat = maxlat = args.lat
-    minlon = maxlon = args.lon
-
     if args.zoom:
         from_zoom = to_zoom = args.zoom
     else:
@@ -59,34 +100,9 @@ def runner(args):
         to_zoom = args.to_zoom
         
     for zoom in range(from_zoom, to_zoom+1):
-        # crazy logic to extend the lat lon bbox and not genereate too many tiles
-        if zoom >= 25:
-            break
-        if args.radius > 1:
-            if zoom < 5:
-                mult = 1.0
-                radius = 40.0
-            elif zoom < 8:
-                mult = 1.0
-                radius = 80.0 / args.radius
-            elif zoom < 13:
-                mult = 1 / zoom
-                radius = args.radius * 20 / zoom 
-            elif zoom < 17:
-                mult = .05 / zoom 
-                radius = args.radius * 20 / zoom 
-            elif zoom > 19:
-                mult = .2
-                radius = .3 * (20 / zoom) /(args.radius * zoom)
-            else: 
-                mult = 0.01 / zoom + 0.0001 * args.radius / zoom
-                radius = args.radius * 20 / zoom 
-            minlat -= (mult * radius)
-            maxlat += (mult * radius)
-            minlon -= (mult * radius)
-            maxlon += (mult * radius)
-
-        seed_layer(minlat, minlon, maxlat, maxlon, zoom, args.url, args.style, args.retina)
+        minlat, minlon, maxlat, maxlon = crazy_extent_calc(zoom)
+        urls = get_seed_urls(minlat, minlon, maxlat, maxlon, zoom, args.url, args.style, args.retina, args.max)
+        print("zoom level: {} = {} ({})".format(zoom, len(urls), urls[0]))
 
 
 if __name__ == "__main__":
