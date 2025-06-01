95439148534d74cb896361510bc34febf0188ce7
Frank Purcell <fxpurcell@gmail.com>
2025-06-01 00:19:24 -0700

    feat(seed): more refactor + curl + script

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -8,16 +8,19 @@ SDIR=`dirname $0`
 
 cd $SDIR
 
+#styles="trimet-satellite-routes rtp"
+styles="trimet trimet-satellite"
 
-# do these once per seed
-# note: the 512 url is one that the ws-st.trimet.org views use (here for demo purposes)
-python -m seed.exe -st -f 1 -t 12 -r 10 -s trimet -b  $*
-python -m seed.exe -st -f 1 -t 12 -r 10 -s trimet-satellite -b $*
-python -m seed.exe -st -f 1 -t 12 -r 10 -s trimet-satellite/512 $*
-python -m seed.exe -st -f 1 -t 12 -r 10 -s trimet/512 $*
+for s in $styles
+do
+  # do these once per seed
+  # note: the 512 url is one that the ws-st.trimet.org views use (here for demo purposes)
+  # example: python -m seed.exe -st -f 1 -t 12 -r 10 -s trimet-satellite -b -c
+  python -m seed.exe -st -f 1 -t 12 -r 10 -s $s -b  $*
+  python -m seed.exe -st -f 1 -t 12 -r 10 -s $s/512 $*
 
-# do these for every point we'll be seeding
-python -m seed.exe -st -f 13 -t 20 -r 5 -s trimet -b $*
-python -m seed.exe -st -f 13 -t 20 -r 5 -s trimet-satellite -b $*
+  # do these for every point we'll be seeding
+  python -m seed.exe -st -f 13 -t 20 -r 5 -s $s -b $*
+done
 
 cd -
